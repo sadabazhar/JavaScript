102187b3cc1e89c06277f6b1dac30dc0584fcaeb
Sadab Azhar <sadabazhar84@gmail.com>
2024-03-09 18:52:11 +0530

    Discuss common loops

---
+++ 01_basics/13_loops.js
@@ -0,0 +1,21 @@
+// for loop
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// while loop
+
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+// Do while loop => Executes first then check condition
+
+let j = 0;
+do {
+    console.log("j is greater then 5");
+    j++;
+} while (i > 5);
